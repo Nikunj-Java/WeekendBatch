9bfb9478e5a1be497ae29a97738e75c42a7c5a71
unknown <ganitniduniyanikunj10@gmail.com>
2022-06-26 20:10:15 +0530

    Session 5 Angular Notes

--- Phase-4/Session-4/angular/myapp/src/app/binding/binding.component.ts
+++ Phase-4/Session-4/angular/myapp/src/app/binding/binding.component.ts
@@ -12,6 +12,12 @@ export class BindingComponent implements OnInit {
   ngOnInit(): void {
   }
   name:string="Nikunj Soni";
-  url:string="https://www.google.com"
+  url:string="https://www.google.com";
+  products=[{name:"pencile",price:"8.3",available:"26-06-2022",rating:4.8},
+            {name:"pen",price:"9.0",available:"30-06-2022",rating:5.0},
+            {name:"Eraser",price:"5.5",available:"01-07-2022",rating:4.5},
+            {name:"Scale",price:"10",available:"27-06-2022",rating:4.8},
+            {name:"Strencil",price:"12.25",available:"26-06-2022",rating:4.9},
+            ]
 
 }
