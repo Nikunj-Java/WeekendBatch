50c1c883afc6c0936bcd43c71fba2bd32eda69cb
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-02 21:42:38 +0530

    Forms and Validation,service and injectable-session 6

--- Phase-4/Session-6/formdemo/src/app/app.module.ts
+++ Phase-4/Session-6/formdemo/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { ServicedemoComponent } from './servicedemo/servicedemo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    ServicedemoComponent
   ],
   imports: [
     BrowserModule,
